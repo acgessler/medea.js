0908f82c47c50950dd8e509490914bbed4ba990d
Alexander Gessler <alexander.gessler@gmx.net>
2011-11-05 17:13:10 +0100

    # remove unintentional unicode character in copyright header

--- medea/medea.filesystem.js
+++ medea/medea.filesystem.js
@@ -2,7 +2,7 @@
 /* medea - an Open Source, WebGL-based 3d engine for next-generation browser games.
  * (or alternatively, for clumsy and mostly useless tech demos written solely for fun)
  *
- * medea is (c) 2011, Alexander Ċ. Gessler 
+ * medea is (c) 2011, Alexander C. Gessler 
  * licensed under the terms and conditions of a 3 clause BSD license.
  */
 
