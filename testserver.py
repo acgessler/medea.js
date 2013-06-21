9ea14f390f4eec16a22074ae084f7862e3cba001
acgessler <alexander.gessler@gmx.net>
2013-06-21 21:53:05 +0200

    Testserver that is Python 3 and Python 2 compatible.

--- testserver.py
+++ testserver.py
@@ -1,4 +1,9 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+import sys
+if sys.version_info <  (3,0):
+	from BaseHTTPServer import HTTPServer
+	from SimpleHTTPServer import SimpleHTTPRequestHandler
+else:
+	from http.server import HTTPServer, SimpleHTTPRequestHandler
  
 httpd = HTTPServer(('127.0.0.1', 80), SimpleHTTPRequestHandler)
 httpd.serve_forever()
