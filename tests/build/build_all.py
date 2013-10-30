56aeefb58037052c55e2dfe056f1a764cf8f18f3
acgessler <alexander.gessler@gmx.net>
2013-10-30 16:43:49 +0100

    add environment to run tests on different permutations of compiler settings.

---
+++ tests/build/build_all.py
@@ -0,0 +1,20 @@
+#!/bin/env python
+
+import sys
+import os
+	# must come first or python's compiler module is picked
+	# definitely must rename the medea compiler module
+sys.path.insert(0, os.path.join('..','..'))
+
+import compiler 
+
+configs = [
+	'null-debug'
+] 
+
+if __name__ == '__main__':
+	for cfg in configs:
+		file = 'compile-config-' + cfg + '.txt'
+		cfg_data = compiler.read_config(file)
+		print cfg_data
+		compiler.run(os.path.join('..', '..', 'medea'), cfg_data)
