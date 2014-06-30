85df4655f60e9f4d6c3b7a70e5a86fc09990ec44
acgessler <alexander.gessler@gmx.net>
2014-06-30 17:54:03 +0200

    Add debug checks to render state handlers to catch invalid values.

--- medea/medea.renderstate.js
+++ medea/medea.renderstate.js
@@ -50,23 +50,77 @@ medealib.define('renderstate',[],function(medealib, undefined) {
 		'one' : gl.ONE
 	};
 
+	// List of supported render states along with how they map to GL.
+	//
+	// Hardcoded, clear error messages here. It is easy to get those
+	// states wrong, and very tedious to debug.
 	var action_map = {
-		'depth_test'  :  function(v) { setsimple(gl.DEPTH_TEST,v); },
-		'depth_write' :  function(v) {
-			gl.depthMask(!!v);
+		'depth_test'  :  function(v) {
+			// #ifdef DEBUG
+			// To catch otherwise hard to find mistakes, only allow |true| and |false|.
+			medealib.DebugAssert(v === true || v === false,
+				"depth_test must be |true| or |false|: " + v);
+			// #endif
+			setsimple(gl.DEPTH_TEST,v);
 		},
+
+		'depth_write' :  function(v) {
+			// #ifdef DEBUG
+			// To catch otherwise hard to find mistakes, only allow |true| and |false|.
+			medealib.DebugAssert(v === true || v === false,
+				"depth_write must be |true| or |false|: " + v);
+			// #endif
+			gl.depthMask(v);
+		},
+
 		'depth_func'  :  function(v) {
+			// #ifdef DEBUG
+			medealib.DebugAssert(df_table[v] !== undefined,
+				"Invalid depth_func: " + v);
+			// #endif
 			gl.depthFunc(df_table[v]);
 		},
 
-		'cull_face'  :  function(v) { setsimple(gl.CULL_FACE,v); },
+		'cull_face'  :  function(v) {
+			// #ifdef DEBUG
+			// To catch otherwise hard to find mistakes, only allow |true| and |false|.
+			medealib.DebugAssert(v === true || v === false,
+				"cull_face must be |true| or |false|: " + v);
+			// #endif
+			setsimple(gl.CULL_FACE,v);
+		},
+
 		'cull_face_mode'  :  function(v) {
+			// #ifdef DEBUG
+			medealib.DebugAssert(cfm_table[v] !== undefined,
+				"Invalid cull_face_mode: " + v);
+			// #endif
 			gl.cullFace(cfm_table[v]);
 		},
 
-		'blend' : function(v) { setsimple(gl.BLEND,v); },
+		'blend' : function(v) {
+			// #ifdef DEBUG
+			// To catch otherwise hard to find mistakes, only allow |true| and |false|.
+			medealib.DebugAssert(v === true || v === false,
+				"blend must be |true| or |false|: " + v);
+			// #endif
+			setsimple(gl.BLEND,v);
+		},
+
 		'blend_func' : function(v) {
+			// #ifdef DEBUG
+			medealib.DebugAssert(bf_table[v[0]] !== undefined && bf_table[v[1]] !== undefined,
+				"Invalid blend function: " + v);
+			// #endif
 			gl.blendFunc(bf_table[v[0]], bf_table[v[1]]);
+		},
+
+		'color_mask' : function(v) {
+			// #ifdef DEBUG
+			medealib.DebugAssert(v.length === 4,
+				"Invalid color_mask, expected list of four booleans: " + v);
+			// #endif
+			gl.colorMask(v[0], v[1], v[2], v[3]);
 		},
 	};
 
